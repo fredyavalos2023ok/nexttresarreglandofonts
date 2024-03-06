6377b27fb39347e19e534c6b0f5d7939593ce43d
afredy4 <afredy.ronaldo4@gmail.com>
2024-03-06 17:44:05 -0500

    arreglando cochinada de bug del propio nextjs14 ya le puse el font que quería el imbecil este

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,10 @@
+// fonts.tsx
+
+// Aquí puedes importar la fuente Lusitana
+import LusitanaFont from 'next/font/google';
+
+// Definición de la clase de la fuente Lusitana
+export const lusitana = {
+  className: 'font-lusitana',
+  src: `url(${LusitanaFont})`,
+};

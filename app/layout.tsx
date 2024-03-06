76e5b0d38c19c7f960f7a6e3922864a7702b6f7e
afredy4 <afredy.ronaldo4@gmail.com>
2024-03-06 18:39:06 -0500

    hasta cuando están solo el layout.app de app y el page.tsx de app  no sale ningún error y tuve que poner el fonts.tsx creado para que no de error acme-logo.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <h1>Este es el Layout general de App</h1>
+        {children}
+      </body>
     </html>
   );
 }
